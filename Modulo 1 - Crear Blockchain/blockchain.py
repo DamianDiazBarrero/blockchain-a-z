86e05236157d84c23a4eb885d83f635938531820
DamianDiazBarrero <damiandiazbarrero@gmail.com>
2022-01-07 13:03:49 +0100

    Update blockchain.py

--- Modulo 1 - Crear Blockchain/blockchain.py
+++ Modulo 1 - Crear Blockchain/blockchain.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Jan  7 10:48:57 2022
 
-@author: encof
+@author: DamianDiazBarrero
 """
 
 # Módulo 1 crear blockchain
@@ -19,3 +19,20 @@ class Blockchain:
     def __init__(self):
         self.chain =[]
         self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index' : len(self.chain)+1, 
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash' : previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]#[-1] se obtiene el primer elemento de la cadena empezando por el final
+    
+    def proof_of_work(self, previous_proof):
+       new_proof = 1
+       check_proof = False
+       while check_proof is False:
+           
