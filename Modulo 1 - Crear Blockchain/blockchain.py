a102e62f07e60f222a169fed024387266dae6e56
DamianDiazBarrero <damiandiazbarrero@gmail.com>
2022-01-07 11:43:55 +0100

    Update blockchain.py

--- Modulo 1 - Crear Blockchain/blockchain.py
+++ Modulo 1 - Crear Blockchain/blockchain.py
@@ -5,3 +5,17 @@ Created on Fri Jan  7 10:48:57 2022
 @author: encof
 """
 
+# Módulo 1 crear blockchain
+
+#Importar librerías
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify 
+
+#Crear blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain =[]
+        self.create_block(proof = 1, previous_hash = '0')
